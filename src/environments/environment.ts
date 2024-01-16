49bf61d994af7dea453fc3ea38fa520b81e3e899
FelipeStefan0 <felipeopdeop@gmail.com>
2024-01-16 20:22:28 -0300

    support: configurando server no environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,11 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+let server = "http://localhost:8080/api"
+
 export const environment = {
-  production: false
+  production: false,
+  server: server
 };
 
 /*
