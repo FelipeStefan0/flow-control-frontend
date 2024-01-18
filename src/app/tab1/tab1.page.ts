b35a4d3fe0666f01b01ddc2c6ab5b969ca910ac6
FelipeStefan0 <felipeopdeop@gmail.com>
2024-01-18 19:47:41 -0300

    support: atualização dinâmica do mês atual

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,8 +1,4 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, inject } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { MaskitoElementPredicateAsync, MaskitoOptions } from '@maskito/core';
-import { Observable } from 'rxjs';
+import { Component } from '@angular/core';
 import { Month } from '../models/Month';
 
 @Component({
@@ -11,7 +7,6 @@ import { Month } from '../models/Month';
   styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
-
   year!: number;
   month!: string;
 
@@ -19,12 +14,15 @@ export class Tab1Page {
 
   ngOnInit() {
     this.year = new Date().getFullYear();
-    // this.month = this.getMonthToString();
+    this.month = this.getMonthToString();
   }
 
-  getMonthToString() {
-    let value = Object.keys(Month).length;
+  getMonthToString(): string {
     let currentMonth = new Date().getMonth();
+    let value = Object.values(Month).length;
+    for (let i = 0; i < value; i++)
+      if (Object.values(Month).indexOf(Object.values(Month)[i]) == currentMonth)
+        return Object.values(Month)[i].toString();
+    return "";
   }
-
 }
