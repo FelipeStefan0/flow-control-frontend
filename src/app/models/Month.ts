29b7b1adaeb15251cef5b4037b106d89385490e8
Felipe Stefano <felipe.stefano@parnamirim.rn.gov.br>
2024-01-18 10:54:40 -0300

    support: enum de month

---
+++ src/app/models/Month.ts
@@ -0,0 +1,14 @@
+export enum Month {
+    JANERIO = "Janeiro",
+    FEVEREIRO = "Fevereiro",
+    MARCO = "Março",
+    ABRIL = "Abril",
+    MAIO = "Maio",
+    JUNHO = "Junho",
+    JULHO = "Julho",
+    AGOSTO = "Agosto",
+    SETEMBRO = "Setembro",
+    OUTUBRO = "Outubro",
+    Novembro = "Novembro",
+    DEZEMBRO = "Dezembro",
+}
