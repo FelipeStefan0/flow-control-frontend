1c8d945ffa9da6c9ee1a4abc861380896c1562d1
Felipe Stefano <felipe.stefano@parnamirim.rn.gov.br>
2024-01-18 10:58:24 -0300

    wip: inicialização da tela inicial

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -1,23 +1,17 @@
 import { IonicModule } from '@ionic/angular';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
 
 import { Tab1PageRoutingModule } from './tab1-routing.module';
-import { HttpClientModule } from '@angular/common/http';
-
-import { MaskitoModule } from '@maskito/angular';
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
-    ReactiveFormsModule,
     Tab1PageRoutingModule,
-    HttpClientModule,
-    MaskitoModule
   ],
   declarations: [Tab1Page]
 })
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -3,48 +3,28 @@ import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MaskitoElementPredicateAsync, MaskitoOptions } from '@maskito/core';
 import { Observable } from 'rxjs';
+import { Month } from '../models/Month';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
 
-  form!: FormGroup;
-  actions$!: Observable<any>;
-  params!: any[];
-
-  fb = inject(FormBuilder);
-  http = inject(HttpClient)
+  year!: number;
+  month!: string;
 
   constructor() {}
 
   ngOnInit() {
-    this.initForm();
-    this.list();
+    this.year = new Date().getFullYear();
+    // this.month = this.getMonthToString();
   }
 
-  initForm() {
-    this.form = this.fb.group({
-      amount: [null, [Validators.required]],
-      notes: [null, [Validators.required]],
-      types: [null, [Validators.required]]
-    })
+  getMonthToString() {
+    let value = Object.keys(Month).length;
+    let currentMonth = new Date().getMonth();
   }
 
-  create() {
-    this.params = this.form.value;
-    this.http.post('http://localhost:8080/api/action', this.params).subscribe(() => this.list());
-  }
-
-  list() {
-    this.actions$ = this.http.get<any[]>('http://localhost:8080/api/action');
-  }
-
-  readonly amountMask: MaskitoOptions = {
-    mask: ['R$', ' ', /\d{5}/, '.', /\d/, /\d/]
-  }
-
-  readonly maskPredicate: MaskitoElementPredicateAsync = async (el) => (el as HTMLIonInputElement).getInputElement();
 }
