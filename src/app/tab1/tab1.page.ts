ab082c7209b685b77405eb38354593cc9205862c
FelipeStefan0 <felipeopdeop@gmail.com>
2024-02-28 22:36:18 -0300

    wip: doing datepicker

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Month } from '../models/Month';
+import { Report } from '../models/Report';
 
 @Component({
   selector: 'app-tab1',
@@ -12,7 +13,20 @@ export class Tab1Page {
 
   openedDatepicker: boolean = false;
 
-  constructor() {}
+  report: Report[] = [];
+  reportOne: Report = {
+    id: 0,
+    year: 2024,
+    month: Month.FEBRUARY,
+    total_value: 250.00,
+    in_total_value: 300.00,
+    out_total_value: 50.00,
+    actions: []
+  }
+
+  constructor() {
+    this.report.push(this.reportOne)
+  }
 
   ngOnInit() {
     this.year = new Date().getFullYear();
