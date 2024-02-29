822ee94ed57a7714a95d481f9f83346a8ee63cdf
FelipeStefan0 <felipeopdeop@gmail.com>
2024-02-28 22:36:57 -0300

    support: changing interfaces and enum

--- src/app/services/action.service.ts
+++ src/app/services/action.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
-import { Action } from '../models/Actions';
+import { Action } from '../models/Action';
 
 @Injectable({
   providedIn: 'root'
