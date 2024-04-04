25a5fd70ef8d2a886ecaefadab7814f5d8a9b8ad
FelipeStefan0 <felipeopdeop@gmail.com>
2024-04-03 21:08:19 -0300

    wip: criação do componente report card

---
+++ src/app/components/report-card/report-card.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-report-card',
+  templateUrl: './report-card.component.html',
+  styleUrls: ['./report-card.component.scss'],
+})
+export class ReportCardComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
