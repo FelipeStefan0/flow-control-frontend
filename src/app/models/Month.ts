5f0d147fc14e01103328fa419689d13f5f94653f
FelipeStefan0 <felipeopdeop@gmail.com>
2024-04-03 23:04:49 -0300

    support: created function to recive value and return enum value

--- src/app/models/Month.ts
+++ src/app/models/Month.ts
@@ -10,5 +10,25 @@ export enum Month {
     SEPTEMBER = "Setembro",
     OCTOBER = "Outubro",
     NOVEMBER = "Novembro",
-    DECEMBER = "Dezembro",
+    DECEMBER = "Dezembro"
+}
+
+export class MonthUtils {
+    translateEnumName(enumValue: string): string {
+        switch(enumValue) {
+            case "Janeiro": return "JANUARY"
+            case "Fevereiro": return "FEBRUARY"
+            case "Março": return "MARCH"
+            case "Abril": return "APRIL"
+            case "Maio": return "MAY"
+            case "Junho": return "JUNE"
+            case "Julho": return "JULY"
+            case "Agosto": return "AUGUST"
+            case "Setembro": return "SEPTEMBER"
+            case "Outubro": return "OCTOBER"
+            case "Novembro": return "NOVEMBER"
+            case "Dezembro": return "DECEMBER"
+            default: return ""
+        }
+    }
 }
