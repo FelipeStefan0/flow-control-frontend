d9764a2c383bd43720a2f5426aaecd5442440198
FelipeStefan0 <felipeopdeop@gmail.com>
2024-04-03 21:07:55 -0300

    feat: finalização do cmponente datepicker

--- src/app/components/datepicker/datepicker.component.ts
+++ src/app/components/datepicker/datepicker.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
+import { Month } from 'src/app/models/Month';
 
 @Component({
   selector: 'app-datepicker',
@@ -13,16 +14,47 @@ import { IonicModule } from '@ionic/angular';
   ]
 })
 export class DatepickerComponent {
+  @Output() emitFilterValue: EventEmitter<any> = new EventEmitter<any>();
   openedDatepicker: boolean = false;
 
+  currentMonth: string = "";
+  currentYear!: number;
+  
+  filter = {
+    month: "",
+    year: 0
+  }
+
   constructor() { }
+
+  ngOnInit() {
+    this.currentYear = new Date().getFullYear();
+    this.currentMonth = this.getMonthToString(new Date().getMonth());
+    this.filter.year = this.currentYear;
+    this.filter.month = this.currentMonth;
+  }
+
+  getMonthToString(monthNumber: number): string {
+    let value = Object.values(Month).length;
+    for (let i = 0; i < value; i++)
+      if (Object.values(Month).indexOf(Object.values(Month)[i]) == monthNumber)
+        return Object.values(Month)[i].toString();
+    return "";
+  }
 
   openDatepicker() {
     this.openedDatepicker = !this.openedDatepicker;
   }
 
-  getDataFromMonthAndYear(event: Event) {
-    console.log(event);
+  getDataFromMonthAndYear(event: any) {
+    if(event.detail.value) {
+      this.filter.month = this.getMonthToString((+(event.detail.value as string).substring(5, 7))-1);
+      this.filter.year = +(event.detail.value as string).substring(0,4)
+    } else {
+      this.filter.month = this.currentMonth;
+      this.filter.year = this.currentYear;
+    }
+    this.emitFilterValue.emit(this.filter)
     this.openDatepicker();
   }
 }
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -8,35 +8,9 @@ import { Report } from '../models/Report';
   styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
-  year!: number;
-  month!: string;
+  constructor() {}
 
-  report: Report[] = [];
-  reportOne: Report = {
-    id: 0,
-    year: 2024,
-    month: Month.FEBRUARY,
-    total_value: 250.00,
-    in_total_value: 300.00,
-    out_total_value: 50.00,
-    actions: []
-  }
-
-  constructor() {
-    this.report.push(this.reportOne)
-  }
-
-  ngOnInit() {
-    this.year = new Date().getFullYear();
-    this.month = this.getMonthToString();
-  }
-
-  getMonthToString(): string {
-    let currentMonth = new Date().getMonth();
-    let value = Object.values(Month).length;
-    for (let i = 0; i < value; i++)
-      if (Object.values(Month).indexOf(Object.values(Month)[i]) == currentMonth)
-        return Object.values(Month)[i].toString();
-    return "";
+  emitFilterValue(event: any) {
+    console.log(event);
   }
 }
