b2b7a0151039d9d009b8e3d15f8338e44c0a3372
FelipeStefan0 <felipeopdeop@gmail.com>
2024-05-30 20:43:25 -0300

    wip: amount filter to remove letters

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,6 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActionService } from '../services/action.service';
+import { map, tap } from 'rxjs';
 
 @Component({
   selector: 'app-tab2',
@@ -16,21 +17,24 @@ export class Tab2Page {
   hiddenCleanButton: boolean = true;
   showCalendar: boolean = false;
 
-  amount!: number;
   date!: string;
-
-  active: boolean = false;
 
   constructor() {}
 
   ngOnInit() {
     this.initForm();
-    let initValueForm = JSON.parse(JSON.stringify(this.form.getRawValue()));
-    this.form.valueChanges.subscribe(() => {
-      if (this.form.getRawValue() != initValueForm) {
-        this.hiddenCleanButton = false;
-      }
-    });
+    // let value;
+    // this.form.get("amount")?.valueChanges.pipe(
+    //   map((res:string) => {
+    //     res = res.replace(/[^0-9\.]+/g, "")
+    //     return res;
+    //   })
+    // ).subscribe((res:string) => {
+    //   console.log("Ola");
+    //   value = res;
+    //   return value;
+    // }) 
+    // this.form.get("amount")?.setValue(value);
   }
 
   initForm() {
@@ -39,6 +43,13 @@ export class Tab2Page {
       hours: ['', [Validators.required]],
       notes: ['', [Validators.required]],
       types: ['', [Validators.required]],
+    });
+
+    let initValueForm = JSON.parse(JSON.stringify(this.form.getRawValue()));
+    this.form.valueChanges.subscribe(() => {
+      if (this.form.getRawValue() != initValueForm) {
+        this.hiddenCleanButton = false;
+      }
     });
   }
 
@@ -50,13 +61,26 @@ export class Tab2Page {
     if (event.detail.value) {
       this.form.get('hours')?.setValue(event.detail.value);
       this.date = event.detail.value;
-    }
-    else {
+    } else {
       this.form.get('hours')?.setValue(new Date().toISOString());
       this.date = new Date().toISOString();
     }
     this.openClose();
   }
+
+  // amountMask() {
+  //   let value;
+  //   this.form.get("amount")?.valueChanges.pipe(
+  //     map((res:string) => {
+  //       res = res.replace(/[^0-9\.]+/g, "")
+  //       return res;
+  //     })
+  //   ).subscribe((res:string) => {
+  //     value = res;
+  //     return value;
+  //   }) 
+  //   this.form.get("amount")?.setValue(value);
+  // }
 
   create() {
     this.service.create(this.form.value).subscribe();
@@ -64,10 +88,7 @@ export class Tab2Page {
 
   clear() {
     this.form.reset();
+    this.date = "";
     this.hiddenCleanButton = true;
-  }
-
-  inputSelected() {
-    this.active = true;
   }
 }
