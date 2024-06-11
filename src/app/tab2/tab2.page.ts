05c9e990cc55130c6a49f817aabc243fb4976a09
FelipeStefan0 <felipeopdeop@gmail.com>
2024-06-11 19:41:14 -0300

    feat: toastr to show message when register a new action

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActionService } from '../services/action.service';
 import { map, tap } from 'rxjs';
+import { Action } from '../models/Interfaces/Action';
 
 @Component({
   selector: 'app-tab2',
@@ -19,16 +20,16 @@ export class Tab2Page {
 
   date!: string;
 
-  actionTypes : {title: string, value: number}[] = [
+  actionTypes: { title: string; value: number }[] = [
     {
-      title: "Receita",
-      value: 0
+      title: 'Receita',
+      value: 0,
     },
     {
-      title: "Despesa",
-      value: 1
-    }
-  ]
+      title: 'Despesa',
+      value: 1,
+    },
+  ];
 
   constructor() {}
 
@@ -44,7 +45,7 @@ export class Tab2Page {
     //   console.log("Ola");
     //   value = res;
     //   return value;
-    // }) 
+    // })
     // this.form.get("amount")?.setValue(value);
   }
 
@@ -69,12 +70,14 @@ export class Tab2Page {
   }
 
   getDate(event: any) {
-    let tzoffset = (new Date()).getTimezoneOffset() * 60000;
+    let tzoffset = new Date().getTimezoneOffset() * 60000;
     if (event.detail.value) {
       this.form.get('date')?.setValue(event.detail.value);
       this.date = event.detail.value;
     } else {
-      this.form.get('date')?.setValue(new Date(Date.now() - tzoffset).toISOString().slice(0, -1));
+      this.form
+        .get('date')
+        ?.setValue(new Date(Date.now() - tzoffset).toISOString().slice(0, -1));
       this.date = new Date().toISOString();
     }
     this.openClose();
@@ -90,17 +93,59 @@ export class Tab2Page {
   //   ).subscribe((res:string) => {
   //     value = res;
   //     return value;
-  //   }) 
+  //   })
   //   this.form.get("amount")?.setValue(value);
   // }
 
   create() {
-    this.service.create(this.form.value).subscribe();
+    this.service.create(this.form.value).subscribe({
+      next: (res: { data: Action[]; message: string; status: number }) => {
+        this.toastr(res.message, 'success');
+      },
+      error: (err: { data: Action[]; message: string; status: number }) => { 
+        this.toastr(err.message, 'failure');
+      },
+      complete: () => {
+        this.clear();
+      },
+    });
   }
 
   clear() {
     this.form.reset();
-    this.date = "";
+    this.date = '';
     this.hiddenCleanButton = true;
+  }
+
+  toastr(message: string, type?: string) {
+    let body: Element = document.getElementsByClassName('ion-padding')[0];
+    let div: HTMLElement = document.createElement('div');
+    let text: HTMLElement = document.createElement('span');
+
+    text.innerHTML = message;
+    div.appendChild(text);
+
+    if (type == 'success') div.style.backgroundColor = 'var(--font-success)';
+    else if (type == 'failure') div.style.backgroundColor = 'var(--font-failure)';
+
+    div.style.color = 'var(--font-tertiary)';
+    div.style.width = '80%';
+    div.style.height = '2.5rem';
+    div.style.padding = '.2rem';
+    div.style.borderRadius = '.5rem';
+    div.style.position = 'absolute';
+    div.style.top = '1.5rem';
+    div.style.left = '50%';
+    div.style.transform = 'translateX(-50%)';
+    div.style.zIndex = '999';
+    div.style.display = 'flex';
+    div.style.justifyContent = 'center';
+    div.style.alignItems = 'center';
+
+    body.appendChild(div);
+
+    setTimeout(() => {
+      body.removeChild(div);
+    }, 3000);
   }
 }
