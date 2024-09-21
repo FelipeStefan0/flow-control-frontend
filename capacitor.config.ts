bde20e188c0c7882f88e29247bc6a47d8e7c159a
FelipeStefan0 <felipemarquesdestefano@gmail.com>
2024-09-21 13:30:47 -0300

    wip: change app icon

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'flowControlFront',
+  appName: 'Controle de Fluxo',
   webDir: 'www',
   server: {
     androidScheme: 'https'
