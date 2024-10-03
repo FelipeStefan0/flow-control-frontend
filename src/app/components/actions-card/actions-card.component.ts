4689d173074743e9da804c65014bdc09a3d0b564
FelipeStefan0 <felipemarquesdestefano@gmail.com>
2024-10-03 13:02:04 -0300

    wip: update and delete modal

--- src/app/components/actions-card/actions-card.component.ts
+++ src/app/components/actions-card/actions-card.component.ts
@@ -2,13 +2,31 @@ import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, OnInit, Output, inject } from '@angular/core';
 import { Action } from 'src/app/models/Interfaces/Action';
 import { ActionService } from 'src/app/services/action.service';
+import {
+  IonModal,
+  IonHeader,
+  IonTitle,
+  IonButtons,
+  IonButton,
+  IonToolbar,
+  IonContent,
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-actions-card',
   templateUrl: './actions-card.component.html',
   styleUrls: ['./actions-card.component.scss'],
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    IonContent,
+    IonToolbar,
+    IonButton,
+    IonButtons,
+    IonTitle,
+    IonHeader,
+    IonModal,
+    CommonModule,
+  ],
 })
 export class ActionsCardComponent implements OnInit {
   @Input() action!: Action;
@@ -17,19 +35,26 @@ export class ActionsCardComponent implements OnInit {
   @Input() description!: string;
   @Input() type!: string;
 
-  @Output() emitEditAction: EventEmitter<{id: number, action: Action}> = new EventEmitter<{id: number, action: Action}>();
+  @Output() emitEditAction: EventEmitter<{ id: number; action: Action }> =
+    new EventEmitter<{ id: number; action: Action }>();
   @Output() emitDeleteAction: EventEmitter<number> = new EventEmitter<number>();
+
+  isModalOpen: boolean = false;
 
   constructor() {}
 
   ngOnInit() {}
 
   editAction(id: number, action: Action) {
-    let event = {id: id, action: action}
+    let event = { id: id, action: action };
     this.emitEditAction.emit(event);
   }
 
   deleteAction(id: number) {
     this.emitDeleteAction.emit(id);
+  }
+
+  openClose() {
+    this.isModalOpen = !this.isModalOpen;
   }
 }
